88968d037f2968794059e72cf8a8e5149e2d87d5
Samuel Cristobal <el.tobal@gmail.com>
2022-08-22 19:14:53 +0200

    a very rude performance test

--- site/index.ts
+++ site/index.ts
@@ -1,5 +1,46 @@
 const js = await import('hello-wasm');
 
-js.greet('WebAssembly with npm');
+// js.greet('WebAssembly with npm');
+
+const fib = function (n: number): number {
+    let res = 1;
+    let prev = 0;
+
+    for (let i = 0; i < n; i++) {
+        const next = prev + res;
+        prev = res;
+        res = next;
+    }
+
+    return res;
+};
+
+const N = 100_000_000;
+
+// test WASM performance
+{
+    const start = performance.now();
+
+    const a = js.fib(N);
+
+    const end = performance.now();
+
+    const timeTaken = end - start;
+
+    console.log('time WASM', timeTaken);
+}
+
+// test JS performance
+{
+    const start = performance.now();
+
+    const b = fib(N);
+
+    const end = performance.now();
+
+    const timeTaken = end - start;
+
+    console.log('time JS', timeTaken);
+}
 
 export {};
--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,17 @@ extern "C" {
 pub fn greet(name: &str) {
     alert(&format!("Hello, {}!", name));
 }
+
+#[wasm_bindgen]
+pub fn fib(num: i32) -> i32 {
+    let mut res = 1;
+    let mut prev = 0;
+
+    for _ in 1..num {
+        let next = prev + res;
+        prev = res;
+        res = next;
+    }
+
+    return res;
+}
