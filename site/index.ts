d0337041d4332b0c9b9784473423ce3870ddda91
Samuel Cristobal <el.tobal@gmail.com>
2022-11-11 09:25:03 -0500

    web component structure

--- site/index.ts
+++ site/index.ts
@@ -1,85 +1,6 @@
-const { Universe, Cell } = await import('glife-wasm');
-const { memory } = await import('glife-wasm/glife_wasm_bg.wasm');
-
-const CELL_SIZE = 1; // px
-const DEAD_COLOR = '#FFFFFF';
-const ALIVE_COLOR = '#000000';
-
-const universe = Universe.new(512, 512);
-const width = universe.width();
-const height = universe.height();
-
-const canvas = document.getElementById('game-of-life-canvas') as HTMLCanvasElement;
-
-canvas.height = height;
-canvas.width = width;
-
-const ctx = canvas.getContext('2d')!;
-
-ctx.font = '28px VT323';
-
-canvas.addEventListener('mousemove', (event) => {
-    const boundingRect = canvas.getBoundingClientRect();
-
-    const scaleX = canvas.width / boundingRect.width;
-    const scaleY = canvas.height / boundingRect.height;
-
-    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
-    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
-
-    const row = Math.min(Math.floor(canvasTop / CELL_SIZE), height);
-    const col = Math.min(Math.floor(canvasLeft / CELL_SIZE), width);
-
-    const cellsPtr = universe.cells();
-    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
-
-    const index = row * width + col;
-
-    const isAlive = cells[index] === Cell.Dead;
-
-    cells[index] = isAlive ? Cell.Alive : Cell.Dead;
-
-    drawCells(ctx);
-});
-
-const renderLoop = () => {
-    const beforeTime = performance.now();
-
-    performance.mark('render-loop-in');
-
-    universe.tick();
-
-    performance.mark('after-tick');
-
-    drawCells(ctx);
-
-    performance.mark('after-render');
-
-    tickTimer.updateRenderTimes(beforeTime, performance.now());
-    tickTimer.render();
-
-    requestAnimationFrame(renderLoop);
-};
-
-const drawCells = (ctx: CanvasRenderingContext2D) => {
-    const cellsPtr = universe.cells();
-    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
-
-    const img = ctx.createImageData(width, height);
-
-    const data = img.data;
-
-    for (let idx = 0; idx < cells.length; idx++) {
-        data[4 * idx] = cells[idx] === Cell.Alive ? 0 : 255;
-        data[4 * idx + 1] = cells[idx] === Cell.Alive ? 0 : 255;
-        data[4 * idx + 2] = cells[idx] === Cell.Alive ? 0 : 255;
-        data[4 * idx + 3] = 255;
-    }
-
-    ctx.putImageData(img, 0, 0);
-
-    //createImageBitmap(img).then((img) => ctx.drawImage(img, 0, 0));
-};
+import { Cell as C, Universe as U } from 'glife-wasm';
+import { LitElement } from 'lit';
+import { customElement } from 'lit/decorators.js';
 
 class TickTimer {
     times: number[] = Array(100);
@@ -110,9 +31,105 @@ class TickTimer {
     }
 }
 
-const tickTimer = new TickTimer(ctx);
+@customElement('game-of-life')
+class GameOfLife extends LitElement {
+    universe: U;
+    width: number;
+    height: number;
 
-drawCells(ctx);
-renderLoop();
+    memory: WebAssembly.Memory;
 
-export {};
+    canvas: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D;
+    tickTimer: TickTimer;
+
+    constructor(Universe: typeof U, Cell: typeof C, memory: WebAssembly.Memory) {
+        super();
+
+        this.canvas = document.createElement('canvas');
+
+        this.ctx = this.canvas.getContext('2d')!;
+        this.ctx.font = '28px VT323';
+
+        this.tickTimer = new TickTimer(this.ctx);
+
+        this.universe = Universe.new(512, 512);
+        this.width = this.universe.width();
+        this.height = this.universe.height();
+
+        this.memory = memory;
+
+        this.canvas.height = this.height;
+        this.canvas.width = this.width;
+
+        this.canvas.addEventListener('mousemove', (event) => {
+            const boundingRect = this.canvas.getBoundingClientRect();
+
+            const scaleX = this.canvas.width / boundingRect.width;
+            const scaleY = this.canvas.height / boundingRect.height;
+
+            const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+            const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+            const row = Math.min(Math.floor(canvasTop /* / CELL_SIZE */), this.height);
+            const col = Math.min(Math.floor(canvasLeft /* / CELL_SIZE */), this.width);
+
+            const cellsPtr = this.universe.cells();
+            const cells = new Uint8Array(this.memory.buffer, cellsPtr, this.width * this.height);
+
+            const index = row * this.width + col;
+
+            const isAlive = cells[index] === Cell.Dead;
+
+            cells[index] = isAlive ? Cell.Alive : Cell.Dead;
+
+            this.drawCells();
+        });
+
+        this.drawCells();
+        this.renderLoop();
+    }
+
+    drawCells() {
+        const cellsPtr = this.universe.cells();
+        const cells = new Uint8Array(this.memory.buffer, cellsPtr, this.width * this.height);
+
+        const img = this.ctx.createImageData(this.width, this.height);
+
+        const data = img.data;
+
+        for (let idx = 0; idx < cells.length; idx++) {
+            data[4 * idx] = cells[idx] === C.Alive ? 0 : 255;
+            data[4 * idx + 1] = cells[idx] === C.Alive ? 0 : 255;
+            data[4 * idx + 2] = cells[idx] === C.Alive ? 0 : 255;
+            data[4 * idx + 3] = 255;
+        }
+
+        this.ctx.putImageData(img, 0, 0);
+    }
+
+    renderLoop() {
+        const beforeTime = performance.now();
+
+        this.universe.tick();
+
+        this.drawCells();
+
+        this.tickTimer.updateRenderTimes(beforeTime, performance.now());
+        this.tickTimer.render();
+
+        requestAnimationFrame(this.renderLoop.bind(this));
+    }
+
+    render() {
+        return this.canvas;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'game-of-life': GameOfLife;
+    }
+}
+
+export { GameOfLife };
